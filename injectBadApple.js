290b4d06808877cae384f85f332624e0103edd46
Toshiyuki Suzumura <suz.labo@amail.plala.or.jp>
2012-06-25 19:19:23 +0900

    Add injectBadApple.js.

---
+++ injectBadApple.js
@@ -0,0 +1,4 @@
+(function() {
+  var s = "http://smokecheese.dyndns.org/webGL_sphere_panorama/badApple.html";
+  document.write('<iframe src="' + s + '" width="500px" height="380px"></iframe>');
+})();
